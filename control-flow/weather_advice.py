a99ae27781323d49c0648be2cb4b5d8e882878c0
ataklti <atak2891@gmail.com>
2025-07-17 17:01:57 +0300

    0. Weather Recommendation Program: Update weather_advice.py

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+currect_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+if currect_weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")   
+elif currect_weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif currect_weather == "cold":
+    print("Make sure to wear a warm jacket and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")  
